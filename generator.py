bc5e450a9f36f068281b15c4e7ddeaeb3861419c
Code-matgre <abbracciaventomatteo@gmail.com>
2026-04-26 10:47:11 +0200

    Introduzione della generazione di mappa
    nuova funzione che genera una mappa con la funzione snoise2d che da dei numeri specifici per poi stamparla con caratteri ascii con una funzione display migliorata

--- generator.py
+++ generator.py
@@ -1,26 +1,51 @@
+from noise import snoise2
 def initialize(length, height):
     matr = []
-    for i in range(height+0.1):
+    for i in range(height):
         row = []
-        for j in range(length+0.1):
+        for j in range(length):
             row.append(0)
         matr.append(row)
     return matr
 
 def incremento(matr, length, height):
     incr = 0
-    for i in range(height+0.1):
-        for j in range(length+0.1):
+    for i in range(height):
+        for j in range(length):
             matr[i][j] = incr
             incr += 0.1
     return matr 
 
 def display(matr, length, height):
-    for i in range(height+0.1):
-        for j in range(length+0.1):
+    for i in range(height):
+        for j in range(length):
             print(f'{matr[i][j]:.1f} ', end=" ")
         print()
     return 0
+def new_display(matr, length, height):
+    for i in range(height):
+        for j in range(length):
+            if matr[i][j] < 0.3:
+                print(f'~', end=" ")
+            elif matr[i][j] < 0.5:
+                print(f'.', end=" ")
+            elif matr[i][j] < 0.8:
+                print(f'*', end=" ")
+            else:
+                print(f'^', end=" ")
+        print()
+    return 0   
 
-matr = incremento(initialize(10, 10), 10, 10)
-display(matr, 10, 10)
+def genera_mappa(matr, length, height, scale):
+    
+    for i in range(height):
+        for j in range(length):
+            valore = snoise2(i/scale, j/scale, octaves=6)
+            matr[i][j] = (valore+1)/2
+    return matr
+
+length = 10
+height = 10
+scale = 10
+matr = genera_mappa(initialize(length, height), length, height, scale)
+new_display(matr, length, height)
